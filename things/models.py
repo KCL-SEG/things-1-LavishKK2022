e9347efe2eda29fb31340393f2be98dd2b17ff40
Lavish Kamal Kumar <klavish382@gmail.com>
2022-12-09 13:24:22 +0000

    bug fix

--- things/models.py
+++ things/models.py
@@ -1,8 +1,13 @@
 from django.core.validators import MaxLengthValidator
 from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
 from django.db import models
+
+def valid_range(value):
+    if value < 0 or value > 100:
+        raise ValidationError();
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True)
-    quantity = models.IntegerField(validators=[MaxLengthValidator(100), MinLengthValidator(0)])
+    quantity = models.IntegerField(blank=False, validators=[valid_range])
